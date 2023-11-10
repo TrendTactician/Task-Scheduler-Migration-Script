c8aa6d782e378d7a0c6cf336c35b0efdc15ed287
Saurabh <87906226+SpQpS@users.noreply.github.com>
2023-11-10 16:52:48 +0530

    Update Task Manager Jobs Migration.ps1

--- Task Manager Jobs Migration.ps1
+++ Task Manager Jobs Migration.ps1
@@ -2,9 +2,13 @@
 $source_server = "Enter_Source_Server_IP"
 $destination_server = "Enter_Destination_Server_IP"
 
-# Set the username and password for the servers
-$username = "Your_Username"
-$password = "Your_Password"
+# Set the credentials for the source server
+$source_username = "Source_Username"
+$source_password = "Source_Password"
+
+# Set the credentials for the destination server
+$destination_username = "Destination_Username"
+$destination_password = "Destination_Password"
 
 # Set the path to the directory containing the XML files for each task
 $export_directory = "C:\Path\To\ExportedTasks"
@@ -12,7 +16,12 @@ $export_directory = "C:\Path\To\ExportedTasks"
 # List all XML files in the export directory
 $xmlFiles = Get-ChildItem -Path $export_directory -Filter *.xml
 
-# Import each task XML file to create the tasks on the destination server
+# Export tasks from the source server
 foreach ($xmlFile in $xmlFiles) {
-    schtasks /create /s $destination_server /u $username /p $password /xml $xmlFile.FullName
+    schtasks /query /s $source_server /u $source_username /p $source_password /fo XML /tn "*" > $xmlFile.FullName
+}
+
+# Import tasks to the destination server
+foreach ($xmlFile in $xmlFiles) {
+    schtasks /create /s $destination_server /u $destination_username /p $destination_password /xml $xmlFile.FullName
 }
